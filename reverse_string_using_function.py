8cf04703289047d152b4681eeaa4e31fa74c5ac7
PratheepanBernier <108440591+PratheepanBernier@users.noreply.github.com>
2022-07-31 19:02:53 +0530

    Add files via upload

---
+++ reverse_string_using_function.py
@@ -0,0 +1,5 @@
+def reverse_string(str1) :
+    reverse1= str1[::-1]
+    return reverse1
+str1=input("Enter the string that needed to be reversed : ")
+print("Reversed string is : ",reverse_string(str1))
